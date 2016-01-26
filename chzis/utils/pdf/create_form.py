6d156b5a88a7c7ad2ab0926c45701f84007e0ff1
WeriYard <adamer.sk@gmail.com>
2016-01-26 09:12:36 +0100

    improve crossboc class, add method to selecy and unselect checbox

--- chzis/utils/pdf/create_form.py
+++ chzis/utils/pdf/create_form.py
@@ -15,30 +15,46 @@ from reportlab.graphics import shapes
 
 
 class MyCrossBox(signsandsymbols.Crossbox):
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0, y=0, size=8, oversize=0, selected=False):
         signsandsymbols.Crossbox.__init__(self)
         self.x = x
         self.y = y
+        self.size = size
+        self._oversize = oversize
+        if selected:
+            self.select()
+        else:
+            self.unselect()
+
+    def select(self):
+        self.crosswidth = 1
+        self.crossColor = colors.black
+        self._oversize = 2
+
+    def unselect(self):
+        self.crosswidth = 0
+        self.crossColor = colors.white
+        self._oversize = -1
 
     def draw(self):
-        # general widget bits
         s = float(self.size)  # abbreviate as we will use this a lot
         g = shapes.Group()
 
-        # crossbox specific bits
         box = shapes.Rect(self.x + 1, self.y + 1, s - 2, s - 2,
                           fillColor=self.fillColor,
                           strokeColor=self.strokeColor,
                           strokeWidth=0.6)
         g.add(box)
 
-        crossLine1 = shapes.Line(self.x + (s * 0.15), self.y + (s * 0.15), self.x + (s * 0.85), self.y + (s * 0.85),
+        s += self._oversize
+
+        crossLine1 = shapes.Line(self.x + (s * 0.15) - self._oversize , self.y + (s * 0.15) - self._oversize , self.x + (s * 0.85), self.y + (s * 0.85),
                                  fillColor=self.crossColor,
                                  strokeColor=self.crossColor,
                                  strokeWidth=self.crosswidth)
         g.add(crossLine1)
 
-        crossLine2 = shapes.Line(self.x + (s * 0.15), self.y + (s * 0.85), self.x + (s * 0.85), self.y + (s * 0.15),
+        crossLine2 = shapes.Line(self.x + (s * 0.15) - self._oversize , self.y + (s * 0.85) , self.x + (s * 0.85), self.y + (s * 0.15) - self._oversize,
                                  fillColor=self.crossColor,
                                  strokeColor=self.crossColor,
                                  strokeWidth=self.crosswidth)
@@ -53,154 +69,127 @@ pdfmetrics.registerFont(TTFont('Arial', 'Arial.ttf'))
 pdfmetrics.registerFont(TTFont('Arial_Italic', 'Arial_Italic.ttf'))
 pdfmetrics.registerFont(TTFont('Verdana_Bold', 'Verdana_Bold.ttf'))
 
-page_size = (88.5 * mm, 140 * mm)
-doc = SimpleDocTemplate("form_letter.pdf", pagesize=page_size,
-                        rightMargin=0,
-                        leftMargin=0,
-                        topMargin=0,
-                        bottomMargin=0)
+def create_meeting_task_card(meeting_item=None, school_class=None):
+    page_size = (88.5 * mm, 140 * mm)
+    doc = SimpleDocTemplate("form_letter.pdf", pagesize=page_size,
+                            rightMargin=0,
+                            leftMargin=0,
+                            topMargin=0,
+                            bottomMargin=0)
 
-header_style = ParagraphStyle(name="base",
-                              fontName="Arial_Bold",
-                              alignment=TA_CENTER,
-                              fontSize=12.1,
-                              rightIndent=-4,
-                              leftIndent=-4,
-                              leading=14,
-                              )
+    header_style = ParagraphStyle(name="base",
+                                  fontName="Arial_Bold",
+                                  alignment=TA_CENTER,
+                                  fontSize=12.1,
+                                  rightIndent=-4,
+                                  leftIndent=-4,
+                                  leading=14,
+                                  )
 
-introduction_style = ParagraphStyle(name="base",
-                                    fontName="Arial_Bold",
-                                    alignment=TA_LEFT,
-                                    fontSize=11.8,
-                                    rightIndent=0,
-                                    leftIndent=5,
-                                    leading=26,
-                                    )
+    introduction_style = ParagraphStyle(name="base",
+                                        fontName="Arial_Bold",
+                                        alignment=TA_LEFT,
+                                        fontSize=11.8,
+                                        rightIndent=0,
+                                        leftIndent=5,
+                                        leading=26,
+                                        )
 
-footer_style = ParagraphStyle(name="base",
-                              fontName="Arial",
-                              alignment=TA_LEFT,
-                              fontSize=8,
-                              rightIndent=0,
-                              leading=10,
-                              leftIndent=5,
-                              allowOrphans = 1,
-                              splitLongWords = 1
-                              )
-
-annonation_style = ParagraphStyle(name="base",
+    footer_style = ParagraphStyle(name="base",
                                   fontName="Arial",
                                   alignment=TA_LEFT,
-                                  fontSize=7.5,
+                                  fontSize=8,
                                   rightIndent=0,
                                   leading=10,
                                   leftIndent=5,
+                                  allowOrphans = 1,
+                                  splitLongWords = 1
                                   )
 
-page_content = []
+    annonation_style = ParagraphStyle(name="base",
+                                      fontName="Arial",
+                                      alignment=TA_LEFT,
+                                      fontSize=7.5,
+                                      rightIndent=0,
+                                      leading=10,
+                                      leftIndent=5,
+                                      )
 
-left_col_margin = 19
-right_col_margin = 152
+    page_content = []
 
-page_content.append(Spacer(0, 5 * mm))
-header = Paragraph("<strong>ZADANIE PODCZAS ZEBRANIA CHRZESCIJAŃSKIEGO ŻYCIA I SŁUŻBY</strong>", header_style)
-page_content.append(header)
-page_content.append(Spacer(0, 8 * mm))
-user_name = Paragraph("Imię i nazwisko:<font name='Arial' size='9'>........................................................</font>", introduction_style)
-page_content.append(user_name)
-slave_user = Paragraph(
-    "<para>Pomocnik(-ca):<font name='Arial' size='9'>..........................................................</font></para>",
-    introduction_style)
-page_content.append(slave_user)
-date = Paragraph("Data:<font name='Arial' size='9'>................................................................................</font>", introduction_style)
-page_content.append(date)
-property = Paragraph("Cecha przemawiania:.<font name='Arial' size='9'>...........................................</font>", introduction_style)
-page_content.append(property)
-page_content.append(Spacer(0, 5 * mm))
+    left_col_margin = 19
+    right_col_margin = 152
 
-d = Drawing(10, 15)
-d.add(String(5, 1, "Zadanie", fontName="Arial_Bold", fontSize=9))
-page_content.append(d)
+    page_content.append(Spacer(0, 5 * mm))
+    header = Paragraph("<strong>ZADANIE PODCZAS ZEBRANIA CHRZESCIJAŃSKIEGO ŻYCIA I SŁUŻBY</strong>", header_style)
+    page_content.append(header)
+    page_content.append(Spacer(0, 8 * mm))
+    user_name = Paragraph("Imię i nazwisko:<font name='Arial' size='9'>........................................................</font>", introduction_style)
+    page_content.append(user_name)
+    slave_user = Paragraph(
+        "<para>Pomocnik(-ca):<font name='Arial' size='9'>..........................................................</font></para>",
+        introduction_style)
+    page_content.append(slave_user)
+    date = Paragraph("Data:<font name='Arial' size='9'>................................................................................</font>", introduction_style)
+    page_content.append(date)
+    property = Paragraph("Cecha przemawiania:.<font name='Arial' size='9'>...........................................</font>", introduction_style)
+    page_content.append(property)
+    page_content.append(Spacer(0, 5 * mm))
 
-d = Drawing(20, 13)
-crossbox = MyCrossBox(left_col_margin)
-crossbox.size = 8
-crossbox.crosswidth = 0.2
-#crossbox.crossColor = colors.white
-d.add(crossbox)
-d.add(String(15 + left_col_margin, 1, "Czytanie Biblii", fontName="Arial", fontSize=9))
-d.add(String(136, 14, "Zadanie przedstawisz", fontName="Arial_Bold", fontSize=9))
-d.add(String(136, 1, "w sali:", fontName="Arial_Bold", fontSize=9))
-page_content.append(d)
+    d = Drawing(10, 15)
+    d.add(String(5, 1, "Zadanie", fontName="Arial_Bold", fontSize=9))
+    page_content.append(d)
 
-d = Drawing(20, 13)
-crossbox = MyCrossBox(left_col_margin)
-crossbox.size = 8
-crossbox.crosswidth = 0.4
-crossbox.crossColor = colors.white
-d.add(crossbox)
-d.add(String(15 + left_col_margin, 1, "Pierwsza rozmowa", fontName="Arial", fontSize=9))
+    d = Drawing(20, 13)
+    d.add(MyCrossBox(left_col_margin, selected=True if meeting_item == "Bible Reading" else False))
+    d.add(String(15 + left_col_margin, 1, "Czytanie Biblii", fontName="Arial", fontSize=9))
+    d.add(String(136, 14, "Zadanie przedstawisz", fontName="Arial_Bold", fontSize=9))
+    d.add(String(136, 1, "w sali:", fontName="Arial_Bold", fontSize=9))
+    page_content.append(d)
 
-crossbox1 = MyCrossBox(150)
-crossbox1.size = 8
-crossbox1.crosswidth = 0.4
-crossbox1.crossColor = colors.white
-d.add(crossbox1)
-d.add(String(15 + right_col_margin, 1, "głównej", fontName="Arial", fontSize=9))
+    d = Drawing(20, 13)
+    d.add(MyCrossBox(left_col_margin, selected=True if meeting_item == "Initial Call" else False))
+    d.add(String(15 + left_col_margin, 1, "Pierwsza rozmowa", fontName="Arial", fontSize=9))
 
-page_content.append(d)
-d = Drawing(20, 13)
-crossbox = MyCrossBox(left_col_margin)
-crossbox.size = 8
-crossbox.crosswidth = 0.4
-crossbox.crossColor = colors.white
-d.add(crossbox)
-d.add(String(15 + left_col_margin, 1, "Odwiedziny ponowne", fontName="Arial", fontSize=9))
+    d.add(MyCrossBox(right_col_margin - 2, selected=True if school_class == 1 else False))
+    d.add(String(15 + right_col_margin, 1, "głównej", fontName="Arial", fontSize=9))
 
-crossbox1 = MyCrossBox(150)
-crossbox1.size = 8
-crossbox1.crosswidth = 0.4
-crossbox1.crossColor = colors.white
-d.add(crossbox1)
-d.add(String(15 + right_col_margin, 1, "drugiej", fontName="Arial", fontSize=9))
+    page_content.append(d)
+    d = Drawing(20, 13)
+    d.add(MyCrossBox(left_col_margin, selected=True if meeting_item == "Return Visit" else False))
+    d.add(String(15 + left_col_margin, 1, "Odwiedziny ponowne", fontName="Arial", fontSize=9))
 
-page_content.append(d)
-d = Drawing(20, 13)
-crossbox = MyCrossBox(left_col_margin)
-crossbox.size = 8
-crossbox.crosswidth = 0.4
-crossbox.crossColor = colors.white
-d.add(crossbox)
-d.add(String(15 + left_col_margin, 1, "Studium biblijne", fontName="Arial", fontSize=9))
+    d.add(MyCrossBox(right_col_margin - 2, selected=True if school_class == 2 else False))
+    d.add(String(15 + right_col_margin, 1, "drugiej", fontName="Arial", fontSize=9))
 
-crossbox1 = MyCrossBox(150)
-crossbox1.size = 8
-crossbox1.crosswidth = 0.4
-crossbox1.crossColor = colors.white
-d.add(crossbox1)
-d.add(String(15 + right_col_margin, 1, "trzeciej", fontName="Arial", fontSize=9))
+    page_content.append(d)
+    d = Drawing(20, 13)
+    d.add(MyCrossBox(left_col_margin, selected=True if meeting_item == "Bible Study" else False))
+    d.add(String(15 + left_col_margin, 1, "Studium biblijne", fontName="Arial", fontSize=9))
 
-page_content.append(d)
-d = Drawing(20, 13)
-crossbox = MyCrossBox(left_col_margin)
-crossbox.size = 8
-crossbox.crosswidth = 0.4
-crossbox.crossColor = colors.white
-d.add(crossbox)
-d.add(String(15 + left_col_margin, 1, "Inne: ................................", fontName="Arial", fontSize=9))
-page_content.append(d)
+    d.add(MyCrossBox(right_col_margin - 2, selected=True if school_class == 3 else False))
+    d.add(String(15 + right_col_margin, 1, "trzeciej", fontName="Arial", fontSize=9))
 
-page_content.append(Spacer(0, 12.5 * mm))
+    page_content.append(d)
+    d = Drawing(20, 13)
+    d.add(MyCrossBox(left_col_margin, selected=True if meeting_item == "Other" else False))
+    d.add(String(15 + left_col_margin, 1, "Inne: ................................", fontName="Arial", fontSize=9))
+    page_content.append(d)
 
-footer = Paragraph(
-    "<para><font name='Arial_Bold'>Wskazówki:</font> Potrzebne informacje dotyczące twojego wystąpie-<br/>nia znajdziesz w miesięczniku <font name='Arial_Italic'>Chrzescijańskie życie i słuzba - pro<br/>gram zebrań.</font>" \
-    " Pracuj nad wskazaną wyżej cechą przemawiania. Została ona opisana w podręczniku Szkoła teokratyczna. <font name='Arial_Bold'>Wez go ze sobą " \
-    " na zebranie chrzescijańskiego życia i służby.</font></para>", footer_style)
-page_content.append(footer)
+    page_content.append(Spacer(0, 12.5 * mm))
 
-page_content.append(Spacer(0, 7.5 * mm))
-annonation = Paragraph("S-89-P 10/15", annonation_style)
-page_content.append(annonation)
+    footer = Paragraph(
+        "<para><font name='Arial_Bold'>Wskazówki:</font> Potrzebne informacje dotyczące twojego wystąpie-<br/>nia znajdziesz w miesięczniku <font name='Arial_Italic'>Chrzescijańskie życie i słuzba - pro<br/>gram zebrań.</font>" \
+        " Pracuj nad wskazaną wyżej cechą przemawiania. Została ona opisana w podręczniku Szkoła teokratyczna. <font name='Arial_Bold'>Wez go ze sobą " \
+        " na zebranie chrzescijańskiego życia i służby.</font></para>", footer_style)
+    page_content.append(footer)
 
-doc.build(page_content)
+    page_content.append(Spacer(0, 7.5 * mm))
+    annonation = Paragraph("S-89-P 10/15", annonation_style)
+    page_content.append(annonation)
+
+    doc.build(page_content)
+
+
+if __name__ == '__main__':
+    create_meeting_task_card()
